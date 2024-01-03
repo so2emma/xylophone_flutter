f37a99e4780f30c8ce814ed7820e4e3c9ab87c8e
so2emma <osoemmanuel1969@gmail.com>
2024-01-03 23:42:37 +0100

    play multiple sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const XylophoneApp());
@@ -6,14 +7,90 @@ void main() {
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note1.wav'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                playSound(1);
+              },
+              child: Container(
+                color: Colors.red,
+                height: 40,
+                width: 100,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                playSound(2);
+              },
+              child: Container(
+                color: Colors.orange,
+                height: 40,
+                width: 100,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                playSound(3);
+              },
+              child: Container(
+                color: Colors.yellow,
+                height: 40,
+                width: 100,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                playSound(4);
+              },
+              child: Container(
+                color: Colors.green,
+                height: 40,
+                width: 100,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                playSound(5);
+              },
+              child: Container(
+                color: Colors.blue,
+                height: 40,
+                width: 100,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                playSound(6);
+              },
+              child: Container(
+                color: Colors.indigo,
+                height: 40,
+                width: 100,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                playSound(7);
+              },
+              child: Container(
+                color: Colors.purple,
+                height: 40,
+                width: 100,
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
